11aed3c14dac2386361327b2c02c6ebad6f2ba5c
JonathanLGeorge <jgeorge12@horizon.csueastbay.edu>
2019-05-24 15:35:18 -0700

    minor changes for heroku proc

--- app/__init__.py
+++ app/__init__.py
@@ -60,15 +60,19 @@ def create_app(config_class=Config):
 			mail_handler.setLevel(logging.ERROR)
 			app.logger.addHandler(mail_handler)
 
-		if not os.path.exists('logs'):
-			os.mkdir('logs')
-		file_handler = RotatingFileHandler('logs/microblog.log',
-										maxBytes=10240, backupCount=10)
-		file_handler.setFormatter(logging.Formatter(
-			'%(asctime)s %(levelname)s: %(message)s '
-			'[in %(pathname)s:%(lineno)d]'))
-		file_handler.setLevel(logging.INFO)
-		app.logger.addHandler(file_handler)
+		if app.config['LOG_TO_STDOUT']:
+			stream_handler = logging.StreamHandler()
+			stream_handler.setLevel(logging.INFO)
+			app.logger.addHandler(stream_handler)
+		else:
+			if not os.path.exists('logs'):
+				os.mkdir('logs')
+			file_handler = RotatingFileHandler('logs/microblog.log',maxBytes=10240, backupCount=10)
+			file_handler.setFormatter(logging.Formatter(
+				'%(asctime)s %(levelname)s: %(message)s '
+				'[in %(pathname)s:%(lineno)d]'))
+			file_handler.setLevel(logging.INFO)
+			app.logger.addHandler(file_handler)
 
 		app.logger.setLevel(logging.INFO)
 		app.logger.info('Microblog startup')
@@ -78,7 +82,7 @@ def create_app(config_class=Config):
 
 @babel.localeselector
 def get_locale():
-	return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+	return 'en'
 
 
 from app import models
