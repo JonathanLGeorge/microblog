d9fd162ea252f34b7127634cc68ccf2d3ff86355
JonathanLGeorge <jgeorge12@horizon.csueastbay.edu>
2019-06-03 00:40:38 -0700

    email reset update

--- tests.py
+++ tests.py
@@ -10,12 +10,15 @@ class TestConfig(Config):
 
 class UserModelCase(unittest.TestCase):
 	def setUp(self):
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+		self.app = create_app(TestConfig)
+		self.app_context = self.app.app_context()
+		self.app_context.push()
 		db.create_all()
 
 	def tearDown(self):
 		db.session.remove()
 		db.drop_all()
+		self.app_context.pop()
 
 	def test_password_hashing(self):
 		u = User(username='susan')
