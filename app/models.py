f1f4eb33e88bc0088e763890a252eed2b61b0582
JonathanLGeorge <32345015+JonathanLGeorge@users.noreply.github.com>
2019-07-25 16:20:55 -0700

    updated string count for posts
    length of posts where to short. added 100 extra

--- app/models.py
+++ app/models.py
@@ -85,7 +85,7 @@ def load_user(id):
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	body = db.Column(db.String(140))
+	body = db.Column(db.String(240))
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	
